62c017761e41c3de5d9bea77d6e290a8b7d9e2a3
BOOBA24 <barryelhadjboubacar19@gmail.com>
2025-06-17 13:05:45 -0400

    i

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from datetime import datetime
 from dotenv import load_dotenv
 import requests
+import pytz  # ✅ Ajout pour gérer les fuseaux horaires
 import uvicorn
 load_dotenv()
 
@@ -11,40 +12,39 @@ CITY = "Toronto"
 API_KEY = 'd2f188f5c57dd17f4bc59c73e0c0fffc'
 
 def get_weather():
-    weather_url =f"http://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}&units=metric"
+    weather_url = f"http://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}&units=metric"
     
     try:
         response = requests.get(weather_url)
         data = response.json()
         
         weather = {
-            'city':data['name'],
-            'temperature':data['main']['temp'],
-            'description':data['weather'][0]['description']
+            'city': data['name'],
+            'temperature': data['main']['temp'],
+            'description': data['weather'][0]['description']
         }
     except Exception as e:
-            print(e)
-            weather = {
-                'city':'N/A',
-                'temperature':'N/A',
-                'description':'N/A'
-            }
+        print(e)
+        weather = {
+            'city': 'N/A',
+            'temperature': 'N/A',
+            'description': 'N/A'
+        }
     return weather
-     
+
 @app.get("/info")
 async def get_info():
-        current_datetime = datetime.now()
-        formatted_date = current_datetime.strftime("%Y-%m-%d")
-        formatted_time = current_datetime.strftime("%H:%M:%S")
+    # ✅ Appliquer fuseau horaire local
+    tz = pytz.timezone("America/Toronto")
+    current_datetime = datetime.now(tz)
 
-        weather = get_weather()
-        
-        return{
-            "date":formatted_date,
-            "time":formatted_time,
-            "weather" : weather
-        }
-          
-                
-        
-        
+    formatted_date = current_datetime.strftime("%Y-%m-%d")
+    formatted_time = current_datetime.strftime("%H:%M:%S")
+
+    weather = get_weather()
+
+    return {
+        "date": formatted_date,
+        "time": formatted_time,
+        "weather": weather
+    }
